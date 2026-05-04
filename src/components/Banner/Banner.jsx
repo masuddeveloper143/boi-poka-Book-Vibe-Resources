002b15fc94370b74e26d4bb3d858feacbb62a27e
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-04 11:39:44 +0800

    Update project

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,15 +3,22 @@ import Bookimage from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-        <div className='flex justify-between items-center p-16 w-full'>
-            <div >
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio explicabo ex ea, aut consectetur tempore repudiandae est dolorum fugit fuga, blanditiis ad officia nemo rem atque eligendi, ipsam necessitatibus placeat!</p>
-                <button className='btn btn-primary'>test test</button>
-            </div>
-            <div>
-                <img className='w-10/12' src={Bookimage} alt="" />
-            </div>
-        </div>
+       <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src={Bookimage}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/components/pages/Bookss.jsx
+++ src/components/pages/Bookss.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { data } from 'react-router';
 
 const Bookss = () => {
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(() => {
+        fetch("booksData.json")
+            .then(res => res.json())
+            .then(data=>{
+                setAllBooks(data)
+            })
+    }, [])
     return (
         <div>
-            <h1>I am Books</h1>
+            <h1 className='text-3xl text-center font-bold'>Books</h1>
         </div>
     );
 };
