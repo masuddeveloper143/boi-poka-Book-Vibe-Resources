eabff1ee7591cc1e07aa2f422112cca7b611f827
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-04 20:00:51 +0800

    Update project

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,8 +1,9 @@
 import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({ sigelBook }) => {
 
-    const { bookName, author, image } = sigelBook;
+    const { bookName, author, image, rating, tags } = sigelBook;
 
 
 
@@ -10,12 +11,18 @@ const Book = ({ sigelBook }) => {
     // console.log(data);
     // console.log(sigelBook);
     return (
-        <div className="card bg-base-100 w-96 shadow-sm border">
-            <figure className='p-3'>
-                <img className='h-[166px]' src={image}
+        <div className="card bg-base-100 w-96 shadow-sm border shadow">
+            <figure className='p-3 bg-gray-100 w-2/3 mx-auto'>
+                <img className='h-[166px] ' src={image}
                     alt="Shoes" />
             </figure>
             <div className="card-body">
+
+                <div className='flex gap-4 justify-center'>
+                    {
+                        tags.map(tag => <button>{tag}</button>)
+                    }
+                </div>
                 <h2 className="card-title">
                     Card Title
                     <div className="badge badge-secondary">NEW</div>
@@ -23,7 +30,7 @@ const Book = ({ sigelBook }) => {
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                 <div className="card-actions justify-end">
                     <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                    <div className="badge badge-outline">{rating} <FaStarHalfAlt /></div>
                 </div>
             </div>
         </div>
