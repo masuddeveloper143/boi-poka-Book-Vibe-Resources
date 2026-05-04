391dabb53045492a4bc4aacf3dc359eb65a21b75
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-04 10:46:51 +0800

    Update project

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Bookimage from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='flex justify-between items-center p-16 w-full'>
+            <div >
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio explicabo ex ea, aut consectetur tempore repudiandae est dolorum fugit fuga, blanditiis ad officia nemo rem atque eligendi, ipsam necessitatibus placeat!</p>
+                <button className='btn btn-primary'>test test</button>
+            </div>
+            <div>
+                <img className='w-10/12' src={Bookimage} alt="" />
+            </div>
         </div>
     );
 };
--- src/components/pages/root/Root.jsx
+++ src/components/pages/root/Root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../navbar/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../../footer/Footer';
+import Banner from '../../Banner/Banner';
 
 const Root = () => {
     return (
@@ -9,6 +10,7 @@ const Root = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
+            
 
         </div>
     );
