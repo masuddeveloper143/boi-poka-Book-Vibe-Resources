e6e29f840e7e11eac5072c9acdbd9ac9f115d00d
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-05 11:52:39 +0800

    Update project

--- src/utility/addToDB.js
+++ src/utility/addToDB.js
@@ -1,5 +1,11 @@
 const getStoredBook = () => {
     const storedBookSTR = localStorage.getItem("readList");
+
+
+    if(storedBookSTR){
+        const StoredBookData = JSON.parse(storedBookSTR)
+        return StoredBookData;
+    }
 }
 
 const addToStoredDB = () => {
